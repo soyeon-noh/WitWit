6e3dbde6d38311467c844ff533ee4ce1926ab13d
soyeon-noh <1010qwerty12@gmail.com>
2021-11-25 17:15:40 +0900

    wit api 구현

--- nodejs/models/user.js
+++ nodejs/models/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const user = Schema({
-  id: String, // 사용자의 고유한 아이디
+  id: String, // 사용자의 고유한 아이디 번호..?
   userId: String, // 사용자가 작성한 아이디
   password: String, // 사용자 비밀번호
   name: String, // 사용자 nickname
