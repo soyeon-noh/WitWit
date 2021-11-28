810495adba96001a36e7ccd95f0d04a88855804e
soparifly <soparifly@naver.com>
2021-11-28 09:04:32 +0900

    folder C mostman test완료

--- nodejs/routes/myroom/folder/folder.ctrl.js
+++ nodejs/routes/myroom/folder/folder.ctrl.js
@@ -2,37 +2,47 @@ import mongoose from "mongoose";
 import FolderSchema from "../../../models/myroom/folder.js";
 import USER from "../../../models/user.js";
 
-const user = {
-  user_id: "test",
+export const folderMain = (req, res) => {
+  FolderSchema.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
 };
+export const folderAdd = async (req, res) => {
+  // user_id 의 folder_id 추가
 
-export const folderMain = async (req, res) => {
-  await console.log(req.body);
-  await res.send("hi");
-  // res.json(user);
-};
-export const getFolderById = async (req, res, next) => {
-  //user_id의  폴더가져오기 findAll
+  const userId = req.params.user_id;
+
+  const folderName = req.body.name;
+  const secret = req.body.secret;
+
+  const folder = new FolderSchema({
+    user_id: userId,
+    folder_name: folderName,
+    secret: secret,
+  });
   try {
-    const users = await USER.find({
-      user_id: req.params.user_id,
-    }).populate("user_id");
-    console.log(users);
-    res.json(users);
-  } catch (error) {
-    console.error(err);
-    next(err);
+    await folder.save();
+    await res.send("sucess");
+  } catch (err) {
+    console.log(err);
   }
 };
-export const ctrlFolder = (req, res) => {
-  //user_id 의 folder 수정삭제 컨트롤러창
-  const { user_id } = req.params;
-};
-export const folderAdd = (req, res) => {
-  // user_id 의 folder_id 추가
-  const { user_id, folder_id } = req.param;
-};
-export const folderDelete = (req, res) => {
-  //user_id 의 folder_id 삭제
-  const { user_id, folder_id } = req.param;
-};
+// export const getFolderById = async (req, res, next) => {
+//   //user_id의  폴더가져오기 findAll
+//   try {
+//     const user = req.body.user_id;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// export const ctrlFolder = (req, res) => {
+//   //user_id 의 folder 수정삭제 컨트롤러창
+//   const { user_id } = req.params;
+// };
+// export const folderDelete = (req, res) => {
+//   //user_id 의 folder_id 삭제
+//   const { user_id, folder_id } = req.param;
+// };
