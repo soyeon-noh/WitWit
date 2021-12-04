09813493d9d74f1d5d64b3d9ed8db763b3dd2d20
soparifly <soparifly@naver.com>
2021-12-04 20:28:53 +0900

    폴더라우터 정리

--- nodejs/routes/myroom/folder/folder.ctrl.js
+++ nodejs/routes/myroom/folder/folder.ctrl.js
@@ -1,34 +1,7 @@
 import folders from "../../../models/myroom/folder.js";
 import wits from "../../../models/wit.js";
 
-const folder_test = [
-  {
-    user_id: "test",
-    folder_id: "test",
-    folder_name: "test",
-    wit_id: [1, 2, 3, 4],
-    secret: true,
-  },
-  {
-    user_id: "test2",
-    folder_id: "test2",
-    folder_name: "test2",
-    wit_id: [1, 2, 3, 4],
-    secret: true,
-  },
-  {
-    user_id: "test3",
-    folder_id: "test3",
-    folder_name: "test3",
-    wit_id: [1, 2, 3, 4],
-    secret: true,
-  },
-];
-// const sample = new folders(folder_test[1]);
-// sample.save();
 export const folderMain = async (req, res) => {
-  const user_id = req.params.user_id;
-  console.log(user_id);
   //   await folders //folders document 생성
   //     .save()
   //     .then(() => {
@@ -38,40 +11,27 @@ export const folderMain = async (req, res) => {
   //       console.error(err);
   //     });
   await folders
-    .find({ user_id: user_id }, function (err, data) {
+    .find({ user_id: req.params.user_id }, function (err, data) {
       res.json(data);
     })
-    .clone(); //일반적으로 쿼리문이 두번실행될수없기때문이 clone()처리를 해준다
+    .clone();
 };
-//CREATE
+
 export const folderAdd = async (req, res) => {
-  // user_id 의 folder_id 추가
-
-  const userId = req.params.user_id;
-
-  const folderName = req.body.folder_name;
-  const secret = req.body.secret;
   const folder = new folders({
-    user_id: userId,
-    folder_name: folderName,
-    secret: secret,
+    user_id: req.body.user_id,
+    folder_name: req.body.folder_name,
+    secret: req.body.secret,
   });
-  await folder.create(req.body);
-  //   await folder.insertOne(folder, function (err, result) {
-  //     res.result("sucess");
-  //   });
-};
-//Update
-//test_wit_id :  499c24d4-c909-439d-b018-a5a09cd400f2
-export const updateFolder = async (req, res) => {
-  //   const { wit_id, user_id, folder_id } = req.params;
-  console.log(req.body);
 
+  await folder.create(req.body);
+};
+
+export const updateFolder = async (req, res) => {
   await wits
     .updateOne(
       { _id: req.body.witId },
       { $set: { folder_id: req.body.folderId } },
-
       function (err, result) {
         if (err) {
           return res.status(500).json(err);
@@ -81,10 +41,18 @@ export const updateFolder = async (req, res) => {
     )
     .clone();
 };
-export const folderDelete = (req, res) => {
-  //user_id 의 folder_id 삭제
-  folders.deleteOne(req.body, function (req, res) {
+
+export const folderDelete = async (req, res) => {
+  await folders.deleteOne(req.body, function (req, res) {
     console.log("삭제완료");
   });
-  res.send("삭제완료");
+  await res.send("삭제완료");
+};
+
+export const folderInfo = async (req, res) => {
+  await wits.find({ folder_id: req.body.folderId }, function (err, result) {
+    if (err) {
+      res.send(err);
+    }
+  });
 };
--- nodejs/routes/myroom/myroom.js
+++ nodejs/routes/myroom/myroom.js
@@ -30,17 +30,9 @@ const user_test = [
 ];
 
 myroom.get("/:user_id", folderCtrl.folderMain);
-
-//Myroom folder 관리
-//추후 로그인정보로 확인하도록 한다:user_id 빼고
-myroom.put("/update", folderCtrl.updateFolder);
-// folder.post("/", folderCtrl.getFolderById);
-// folder.get(
-//   "/:user_id/insert",
-//   folderCtrl.ctrlFolder
-//   //내용물 추가삭제, 정렬을 진행할수 있다.(예정)
-// );
-myroom.post("/:user_id/add", folderCtrl.folderAdd);
-myroom.post("/:user_id/delete/:folder_id", folderCtrl.folderDelete);
+myroom.put("/folderUpdate", folderCtrl.updateFolder);
+myroom.post("/folderInfo", folderCtrl.folderInfo);
+myroom.post("/folderAdd", folderCtrl.folderAdd);
+myroom.post("/folderDelete", folderCtrl.folderDelete);
 
 export default myroom;
