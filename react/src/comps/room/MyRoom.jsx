a83f594cba79c660083e71ee568b3965234fdf8f
dkdud8140 <cho.ay4@gmail.com>
2021-12-14 15:43:19 +0900

    폴더수정하기완료

--- react/src/comps/room/MyRoom.jsx
+++ react/src/comps/room/MyRoom.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Outlet} from "react-router";
+import { Outlet } from "react-router";
 
 import "../../css/myroom/MyRoom.css";
 import RoomWitItem from "./RoomWitItem";
+import WitItem from "../wit/WitItem";
 
-function MyRoom(props) {
+function MyRoom() {
+  // const { setWitList, witList } = useWitContext();
+
+  // // wit list 불러오기
+  // const showWitList = useCallback(async () => {
+  //   const list = await WitFetch();
+  //   await setWitList(list);
+  // }, []);
+  // useEffect(showWitList, [showWitList]);
+
   return (
     <div className="myRoomBox">
       <div className="idCard">유저 프로필 정보 들어감</div>
       <div className="userWitList">
-          <RoomWitItem />
+        <RoomWitItem />
+        {/* <WitItem showWitList={showWitList} witList={witList} /> */}
       </div>
       <Outlet />
     </div>
