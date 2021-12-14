13f084a0dba5674801b30059dcd432f156dff9e6
soyeon-noh <1010qwerty12@gmail.com>
2021-12-14 14:17:59 +0900

    rewit 구현

--- nodejs/models/wit.js
+++ nodejs/models/wit.js
@@ -8,13 +8,16 @@ const Schema = mongoose.Schema;
 
 const wit = Schema({
   id: { type: Number, default: 0 }, // 위트 아이디
-  text: { type: String, required: true }, // 위트 텍스트 (512자 제한)
+  //   text: { type: String, required: true }, // 위트 텍스트 (512자 제한)
+  text: String, // 위트 텍스트 (512자 제한)
   createdDate: String, // 위트 생성 날짜
   createdTime: String, // 위트 생성 시간
   userId: String, // 작성자 ID
   userName: String, // 작성자 이름
   profileUrl: String, // 작성자 프로필 이미지링크
   parentWit: Number, // 이전 위트 id값
+
+  rewitId: Number, // 리위트 id값
 
   folder_id: String, // 폴더 seq (외래키)
   image_id: String, // 이미지 seq (외래키)
