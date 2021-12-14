f1f149207640cca5ad0e2d195b8702e2b51d8d4b
dkdud8140 <cho.ay4@gmail.com>
2021-12-14 15:51:57 +0900

    폴더수정하기정말완료

--- react/src/comps/room/MyRoom.jsx
+++ react/src/comps/room/MyRoom.jsx
@@ -7,12 +7,14 @@ import WitItem from "../wit/WitItem";
 
 function MyRoom() {
   // const { setWitList, witList } = useWitContext();
+
   // // wit list 불러오기
   // const showWitList = useCallback(async () => {
   //   const list = await WitFetch();
   //   await setWitList(list);
   // }, []);
   // useEffect(showWitList, [showWitList]);
+
   return (
     <div className="myRoomBox">
       <div className="idCard">유저 프로필 정보 들어감</div>
