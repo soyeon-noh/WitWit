8990fd21633a6313a12cd1f1e9c9c7a829b3b3d4
dkdud8140 <cho.ay4@gmail.com>
2021-12-15 15:43:30 +0900

    위마크오류

--- react/src/functions/WitFetch.jsx
+++ react/src/functions/WitFetch.jsx
@@ -1,16 +1,13 @@
-
 // wit List 호출하는 Fetch
 // 수정 필요 로그인에 따라 다르게
-export const WitFetch = async()=>{
+export const WitFetch = async () => {
   const res = await fetch("http://localhost:5050/wit");
   const list = await res.json();
-  return list
-}
-
-
+  return list;
+};
 
 //wit Insert
-export const WitInsertFetch = async(wit)=>{
+export const WitInsertFetch = async (wit) => {
   const fetch_option = {
     method: "POST",
     headers: {
@@ -20,11 +17,10 @@ export const WitInsertFetch = async(wit)=>{
   };
 
   await fetch("http://localhost:5050/wit", fetch_option);
-}
-
+};
 
 // wit 삭제 Fetch
-export const WitDelete = async(user_id, id) =>{
+export const WitDelete = async (user_id, id) => {
   const fetch_option = {
     method: "DELETE",
     headers: {
@@ -34,28 +30,37 @@ export const WitDelete = async(user_id, id) =>{
   };
 
   await fetch(`http://localhost:5050/wit/${user_id}/${id}`, fetch_option);
-
-}
+};
 
 // wit search Fetch
-export const WitSearchFetch = async(keyword) =>{
+export const WitSearchFetch = async (keyword) => {
   const result = await fetch(`http://localhost:5050/wit/search?q=${keyword}`);
   const json = await result.json();
-  return json
-}
-
+  return json;
+};
 
 // wit를 folder에 넣기
-export const WitInFolderFetch = async(id,folder_id, list) =>{
+export const WitInFolderFetch = async (id, folder_id, list) => {
   const fetch_option = {
-    method : "POST",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(list),
   };
 
-  await fetch(`http://localhost:5050/wit/${id}/${folder_id}`,fetch_option)
-  
-}
+  await fetch(`http://localhost:5050/wit/${id}/${folder_id}`, fetch_option);
+};
 
+//위마크하기
+export const WitMarkFetch = async (wit) => {
+  const id = wit.id;
+  const fetch_option = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(wit),
+  };
+  await fetch(`http://localhost:5050/wit/wimark/${id}`, fetch_option);
+};
