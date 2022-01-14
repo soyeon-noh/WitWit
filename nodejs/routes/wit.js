2c99ce54690aacbd736baecf338060bc9f9115c4
soyeon-noh <1010qwerty12@gmail.com>
2022-01-15 01:20:14 +0900

    wit ctrl 분리

--- nodejs/routes/wit.js
+++ nodejs/routes/wit.js
@@ -1,41 +1,26 @@
 import express from "express";
-import moment from "moment";
-import WIT from "../models/wit.js";
-import FILE from "../models/file.js";
-import FOLLOW from "../models/follow.js";
-
 import multerUpload from "../modules/fileuploadConfig.js";
-
 import * as witCtrl from "./wit.ctrl.js";
 
 const router = express.Router();
 
 /* GET home page. */
-
 // wit 전체 불러오기
 router.get("/", witCtrl.getAllWit);
-
 // 단순 추가
 router.post("/", multerUpload.array("file"), witCtrl.insertWit);
-
 // 답글 추가
 router.post("/:wit_id", witCtrl.createReply);
-
 // 위마크하기
 router.post("/wimark/:wit_id", witCtrl.wimark);
-
 // 인용하기
 router.post("/quote/:wit_id", witCtrl.quote);
-
 // wit 검색
 router.get("/search", witCtrl.search);
-
 // wit 디테일
 router.get("/:user_id/:wit_id", witCtrl.detail);
-
 // wit 삭제
 router.delete("/:user_id/:wit_id", witCtrl.deleteWit);
-
 // wit 를 folder에 추가
 router.post("/:wit_id/:folder_id", witCtrl.putFolderIntoWit);
 
