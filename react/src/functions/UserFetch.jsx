921e4a2b78dcd19e8dafe65f08705d5cf05b8454
soyeon-noh <1010qwerty12@gmail.com>
2022-01-19 03:51:23 +0900

    fix(front): login 여부에 따른 nav 변화

--- react/src/functions/UserFetch.jsx
+++ react/src/functions/UserFetch.jsx
@@ -1,30 +1,27 @@
-const fetch_option = (user)=> {
-    return {
-        method:"POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body:JSON.stringify(user)
-    }
-}
+const fetch_option = (user) => {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+};
 
+export const LoginFetch = async (user) => {
+  const option = fetch_option(user);
+  const res = await fetch(`/users/login`, option);
+  return res;
+};
 
-export const LoginFetch =async(user) =>{
-    const option = fetch_option(user);
-    const res = await fetch(`/users/login`, option);
-    return res
-}
+// 여기 오타있어요 jogin -> join
+export const JoginFetch = async (user) => {
+  const option = fetch_option(user);
+  await fetch(`/users/join`, option);
+};
 
-
-export const JoginFetch =async(user) =>{
-    const option = fetch_option(user);
-    await fetch(`/users/join`, option);
-}
-
-
-export const UserCheck = async() =>{
-    const res = await fetch(`/users/check`)
-    const user = await res.json()
-    return user
-}
-
+export const UserCheck = async () => {
+  const res = await fetch(`/users/check`);
+  const user = await res.json();
+  return user;
+};
